6805d5e19aafc79f8a4393264f17d892f7d50424
Drew Guarnera <dtg3@zips.uakron.edu>
2014-04-17 15:24:04 -0400

    Add task notes

--- triehard.hpp
+++ triehard.hpp
@@ -6,68 +6,76 @@
 #include <map>
 #include <iostream>
 
+//LATER...
+//TRINODE BASE and TRINODE
+
+// USE IT: LOOKUP MEMBER INITIALIZER LIST
 // Template Params:
 // K, Key V, Value
 template<typename K, typename V>
 struct trieNode {
-	V* value;
-	std::map<K, trieNode<K, V>* > neighbors;
-	
-	trieNode() { value = NULL; }
-	trieNode(const V& val) {
-		value = val;
-	}
+  V* value;
+  std::map<K, trieNode<K, V>* > neighbors;
+  
+  trieNode() { value = NULL; }
+  trieNode(const V& val) {
+    value = val;
+  }
 
-	~trieNode() {
-		delete value;
-	}
-
-	V* getValue() {
-		return value;
-	}
+  ~trieNode() {
+    delete value;
+  }
 };
 
+//MAKE TEMPLATE
 struct trie {
-	trieNode<char, int>* head;
+  // GET VALUE TYPE OF STRING which is Char
+  // NEEDS TO BE A VALUE TYPE
+  trieNode<char, int>* head;
 
-	trie() {
-		head = new trieNode<char, int>;
-	}
+  trie() {
+    head = new trieNode<char, int>;
+  }
 
-	~trie() {
-		delete head;
-	}
+  ~trie() {
+    delete head;
+  }
 
-	void add(const std::string word, const int& val) {
-		trieNode<char, int>* start = head;
-		for (unsigned int i = 0; i < word.length(); ++i) {
-			std::map< char,trieNode<char, int>* >::const_iterator search = start->neighbors.find(word[i]);
-    		
-    		if(search == start->neighbors.end()) {
-    			start->neighbors[word[i]] = new trieNode<char, int>;
-    		}
-    		
-    		start = start->neighbors[word[i]];
-		}
-	}
-	
-	void remove(const std::string word) {}
-	
-	bool find(const std::string word) {
-		trieNode<char, int>* start = head;
-		for (unsigned int i = 0; i < word.length(); ++i) {
-			std::map< char,trieNode<char, int>* >::const_iterator search = start->neighbors.find(word[i]);
-    		if(search == start->neighbors.end())
-    			return false;
-    		
-    		start = start->neighbors[word[i]];
-		}
-		return true;
-	}
+  // ADD CAN FAIL IF IT'S ALREADY THERE. GO BOOL!
+  void add(const std::string& word, const int& val) {
+    trieNode<char, int>* start = head;
+    for (std::size_t i = 0; i < word.length(); ++i) {
+      auto search = start->neighbors.find(word[i]);
+      
+      if(search == start->neighbors.end()) {
+        // USE INSERT INSTEAD
+        start->neighbors[word[i]] = new trieNode<char, int>;
+      }
+    
+      start = start->neighbors[word[i]];
+    }
+  }
+  
+  void remove(const std::string& word) {}
+  
+  // SHOULD BE A COUNT return size_t
+  // 0 or 1 Present or not
+  bool find(const std::string& word) {
+    trieNode<char, int>* start = head;
+    for (std::size_t i = 0; i < word.length(); ++i) {
+      
+      auto search = start->neighbors.find(word[i]);
+      if(search == start->neighbors.end())
+        return false;
+      
+      start = start->neighbors[word[i]];
+    }
+    return true;
+  }
 
-	int* operator[] (const std::string word) {
-		return head->getValue();
-	}
+  int* operator[] (const std::string word) {
+    return head->value;
+  }
 };
 
 #endif
