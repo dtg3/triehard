6ff2b55ef129d81ef8ddfff2b50f72fb2e8a72e0
Drew Guarnera <dtg3@zips.uakron.edu>
2014-04-18 11:53:13 -0400

    Convert all tabs to spaces and set default tab to two spaces

--- triehard.test/init.cpp
+++ triehard.test/init.cpp
@@ -3,14 +3,14 @@
 #include <iostream>
 
 void trie_init() {
-	trie<std::string, int>* test = new trie<std::string, int>;
-	std::cerr << "TRIE CREATION\n";
-	if (not (test->head->value == NULL))
-		throw failed;
+  trie<std::string, int>* test = new trie<std::string, int>;
+  std::cerr << "TRIE CREATION\n";
+  if (not (test->head->value == NULL))
+    throw failed;
 }
 
 int main() {
-	int err = 0;
-	err += test(trie_init);
-	return err;
+  int err = 0;
+  err += test(trie_init);
+  return err;
 }
